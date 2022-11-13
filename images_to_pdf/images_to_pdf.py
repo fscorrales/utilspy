e0d8ac83295d9a0f9985b17f3228d0a0bb954244
Fernando Corrales <corrales_fernando@hotmail.com>
2022-11-13 11:03:29 -0300

    feat: add image_to_pdf.py

---
+++ images_to_pdf/images_to_pdf.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Author: Fernando Corrales <fscorrales@gmail.com>
+Source: clcoding.com
+Youtube source: https://youtu.be/Pv6ioGZeouI
+Purpose: Convert images to pdf
+Require package: pillow
+"""
+
+import argparse
+
+from PIL import Image
+
+# --------------------------------------------------
+def get_args():
+    """Get Filename and Output name from user input"""
+    parser = argparse.ArgumentParser(
+        description = 'Convert images to pdf',
+        formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        'filename', 
+        metavar = 'Filenames',
+        nargs= '+', 
+        default = [],
+        help = 'Filenmanes with extension as list',
+        type=str)
+
+    parser.add_argument(
+        '-o', '--output', 
+        metavar = 'pdf_name', 
+        default = 'output.pdf',
+        help = "pdf's file name",
+        type = str)
+
+    return parser.parse_args()
+
+# --------------------------------------------------
+def image_pdf(filename, output):
+    """Convert list of images to pdf"""
+    images = []
+    for file in filename:
+        im = Image.open(file)
+        im = im.convert('RGB')
+        images.append(im)
+
+        images[0].save(output, save_all = True, append_images = images[1:])
+    #image_pdf(["image1.jpg", "image2.png"], "output.pdf")
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    image_pdf(args.filename, args.output)
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
+    #python images_to_pdf.py test.png test2
